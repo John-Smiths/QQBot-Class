30a16bb42b7b1ee88e4ccfa04bfbd0cb07e83c7b
John-Smiths <171165796+John-Smiths@users.noreply.github.com>
2024-10-02 12:27:16 +0800

    Update downimg.py
    修复了QQ URL策略

--- py/downimg.py
+++ py/downimg.py
@@ -1,5 +1,31 @@
 from os import system
+from urllib.parse import quote, urlparse, urlunparse
+
+def escape_url（url）：
+    # 用于转义
+    system（f'curl -o db/{name}.jpg “{url}”'）
+    
+    query_params = parsed_url.query
+    escaped_query_params = []
+    
+    for param in query_params.split('&'):
+        if '=' in param:
+            key，值 = param。split（'='， 1）
+            escaped_query_params.append(f"{quote(key)}={quote(value)}")
+        else:
+            escaped_query_params.append(quote(param))
+    
+    escaped_url = urlunparse((
+        parsed_url.scheme,
+        parsed_url.netloc,
+        parsed_url.path,
+        parsed_url.params,
+        '&'.join(escaped_query_params),
+        parsed_url.fragment
+    ))
+    
+    return escaped_url
 
 def download(url, name):
-    print("command", "-" * 20, ">", f'curl -o db/{name}.jpg "{url}"')
+    print("command", "-" * 20, ">", f'curl -o db/{name}.jpg "escape_url(url)"')
     system(f'curl -o db/{name}.jpg "{url}"')
